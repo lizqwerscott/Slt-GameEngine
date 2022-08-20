6550e1e62a735e7a825d9cbd2d4633397ce2dbc0
lizqwerscott <lizqwerscott@163.com>
2022-08-20 10:03:54 +0800

    change script class format

--- src/Script/Script.h
+++ src/Script/Script.h
@@ -21,6 +21,13 @@ namespace slt
 {
 class Script
 {
+private:
+    Script();
+    ~Script();
+public:
+    static void Create();
+    static void Destory();
+    static void init(int argc, char *argv[]);
 public:
     static cl_object lisp(const std::string &call);
 public:
@@ -32,18 +39,11 @@ public:
     static void drawUi();
 public:
     static void setDrawUi();
-public:
-    static void Create();
-    static void Destory();
-    static void init(int argc, char *argv[]);
 private:
     char m_inputBuf[256];
     const char * m_commands;
     bool m_open = false;
     bool m_autoScroll = true;
-private:
-    Script();
-    ~Script();
 };
 };
 
